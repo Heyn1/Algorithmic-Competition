b8ccc88946fbb77cafbf17afd2ea100ffd459964
HeyloveFly <dyingren@163.com>
2019-10-09 17:02:59 +0800

    Add files via upload

--- POJ1050.cpp
+++ POJ1050.cpp
@@ -1,37 +1,34 @@
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 #include <cmath>
+#include <climits>
 using namespace std;
-int n;
-int arr[105][105];
-int sum[105][105];
+const int maxn = 105;
+
+int arr[maxn][maxn], ans[maxn][maxn], n, sum;
 
 int main() {
-    scanf("%d", &n);
-    int ans = 0;
-    for (int i = 1; i <= n; ++i) {
-        int tmp = 0;
-        for (int j = 1; j <= n; ++j) {
-            scanf("%d", &arr[i][j]);
-            tmp += arr[i][j];
-            ans = max(ans, tmp);
-            if (tmp < 0)
-                tmp = 0;
-        }
-    }
-    for (int i = 1; i <= n - 1; ++i) {
-        for (int j = i + 1; j <= n; ++j) {
-            int tmp = 0;
-            for (int k = 1; k <= n; ++k) {
-                arr[i][k] += arr[j][k];
-                tmp += arr[i][k];
-                ans = max(ans, tmp);
-                if (tmp < 0)
-                    tmp = 0;
-            }
-        }
-    }
-    printf("%d\n", ans);
-
-    return 0;
+	scanf("%d", &n);
+	int tmp;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			scanf("%d", &tmp);
+			arr[i][j] = tmp + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+		}
+	}
+	int res = INT_MIN;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = i; j <= n; ++j) {
+			sum = 0;
+			for (int k = 1; k <= n; ++k) {
+				sum += arr[j][k] - arr[j][k - 1] - arr[i - 1][k] + arr[i - 1][k - 1];
+				res = max(res, sum);
+				if (sum < 0)
+					sum = 0;
+			}
+		}
+	}
+	printf("%d\n", res);
+	
+	return 0;
 }
